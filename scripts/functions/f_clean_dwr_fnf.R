d0738cd1c6274eb2280e368a3e7240a03487e735
Ryan Peek <rapeek@ucdavis.edu>
2021-11-17 23:41:52 -0800

    update with functions and ffc

--- scripts/functions/f_clean_dwr_fnf.R
+++ scripts/functions/f_clean_dwr_fnf.R
@@ -5,20 +5,81 @@ library(dplyr)
 library(readr)
 library(glue)
 library(contentid)
+library(lubridate)
 library(janitor)
+library(ggplot2)
+library(imputeTS)
+library(wateRshedTools)
 
-f_clean_flow_verona <- function(siteID) {
+f_clean_dwr_fnf <- function(siteID="TLG") {
+  
+  # get data:
+  print("loading raw data...")
   
   # get raw data ID:
-  flow <- contentid::store(glue("data_raw/raw_dwr_fnf_{siteID}.csv"))
+  flowdat <- contentid::store(glue("data_raw/fnf_daily_dwr_{siteID}_downloaded_cdec.csv"))
+  print(glue("The hash for current data is: {flowdat}"))
   
-  flow_file <- contentid::resolve("hash://sha256/ac986e4bba591e050e68705e48dbc9002ce1151158d0bcab8a979c37aaf2b8b3")
+  # this is hard coded so if something changes we need to rerun to get new hash
+  flow_file <- contentid::resolve("hash://sha256/bc46aefc7a96f6ca46652177a124afc793d64ff63cd832891d4e755d15406ba1")
   
   # read in data
-  flowdat <- read_csv(flow_file) %>%
-    clean_names() %>%
-    # filter to 1996 to current
-    filter(date >= as.Date("1996-10-01"))
+  flowdat <- read_csv(flow_file, show_col_types = FALSE) %>%
+    # add water year, dowy columns
+    add_WYD(., "date")
   
-  write_csv(flowdat, file=glue("data_clean/clean_dwr_fnf_{siteID}.csv"))
+  flowdat <- flowdat %>%
+    rename(flow=value) %>% 
+    # replace NAs with -1 to plot
+    mutate(flow_rev = ifelse(is.na(flow), -1, flow))
+  
+  # visualize the negatives
+  g1 <- ggplot() + 
+    geom_col(data=flowdat, aes(x=date, y=flow_rev), 
+             color=ifelse(flowdat$flow_rev>0, "cyan4", "red3")) +
+    labs(title=glue("DWR Full Natural Flow for {siteID}: Raw Data"),
+         caption="Negative values are in red",
+         x="", y="Flow (cfs)") +
+    theme_classic() +
+    scale_y_continuous(labels = scales::comma)
+  print(g1)
+  
+  # Interpolate Values ------------------------------------------------------
+  
+  # use imputeTS: http://steffenmoritz.github.io/imputeTS/
+  
+  clean_df <- flowdat %>%
+    # first fill all negatives with NA then interpolate
+    mutate(flow_na = ifelse(flow<0, NA_integer_, flow),
+           # now interpolate only na's
+           ## structural time series model (Kalman)
+           flow_kalman = imputeTS::na_kalman(flow_na), 
+           # mean average over 7 days
+           flow_ma7 = imputeTS::na_ma(flow_na, k = 7, weighting = "exponential"))
+  
+  ## VISUALIZE --------------------------------------------------------
+  
+  # visualize the nas and interpolated versions
+  #imputeTS::ggplot_na_distribution(clean_df$flow_na)
+  #imputeTS::ggplot_na_distribution(clean_df$flow_kalman)
+  
+  # write out
+  readr::write_csv(clean_df, glue("data_clean/clean_fnf_daily_dwr_{siteID}_cdec.csv"))
+  
+  # print message!
+  print(glue("Data saved here: 'data_clean/clean_fnf_daily_dwr_{siteID}_cdec.csv'"))
+  
+  # quick plot
+  p1 <- ggplot(clean_df) +
+    geom_line(aes(x=date, y=flow_kalman)) +
+    labs(
+      title=glue("DWR Full Natural Flow for {siteID}: Interpolated {min(year(clean_df$date))}-{max(year(clean_df$date))}"),
+      caption="Negative values interpolated with Kalman structural time series {imputeTS}",
+      x="", y="Flow (cfs)") +
+    scale_y_continuous(labels = scales::comma)+
+    theme_classic()
+  print(p1)
+  
+  return(clean_df)
+
 }
--- scripts/functions/f_get_dwr_fnf.R
+++ scripts/functions/f_get_dwr_fnf.R
@@ -1,77 +1,26 @@
-# Download data from DWR FNF
+# Download and clean data from DWR FNF at TLG
 
 library(glue)
-#library(dataRetrieval)
 library(dplyr)
-library(ggplot2)
+library(lubridate)
+library(readr)
 library(janitor)
+library(wateRshedTools) 
+# custom package: devtools::install_github("ryanpeek/wateRshedTools")
+
 
 # pull data: defaults to Verona daily discharge
-f_get_dwr_fnf <- function(siteID) {
+f_get_dwr_fnf <- function(siteID="TLG", sensor=8) {
   
   # get data:
   print("Downloading data...")
-  
-  flowdat <- read_csv('data_raw/TLG_2005_2020.csv',
-                      show_col_types = FALSE,
-                      col_select=c('OBS DATE', 'VALUE', 'DATA_FLAG')) %>%
-    rename(date=1, flow=2) # rename columns
+  flowdat <- get_cdec(siteID, sensor, "D", start="1986-04-01", end="2021-11-01") %>% 
+    mutate(date = as_date(datetime), .after=sensor_type) %>% 
+    select(-datetime)
 
   print("Data downloaded!")
   
-  # make YMD date columns
-  WY2005to2020_FNF$date <- lubridate::ymd(WY2005to2020_FNF$date)
-  
-  # check class
-  class(WY2005to2020_FNF$date) # check class (should be Date)
-  class(WY2005to2020_FNF$flow) # check class (should be numeric)
-  
-  # make clean data version and replace NAs with zero to plot
-  clean_df <- WY2005to2020_FNF %>%
-    mutate(flow_rev = ifelse(is.na(flow), 0, flow))
-  
-  summary(clean_df)
-  
-  (g1 <-ggplot() + geom_col(data=clean_df, aes(x=date, y=flow_rev), color=ifelse(clean_df$flow_rev>0, "blue", "red")))
-  
-  # interactive plot with plotly
-  plotly::ggplotly(g1)
-  
-  # Interpolate Values ------------------------------------------------------
-  
-  # see website for more info on imputeTS:
-  # http://steffenmoritz.github.io/imputeTS/
-  
-  clean_df <- clean_df %>%
-    # first fill all negatives with NA then interpolate
-    mutate(flow_na = ifelse(flow<0, NA_integer_, flow),
-           # now interpolate only na's
-           flow_kalman = imputeTS::na_kalman(flow_na), # structural time series model
-           flow_ma7 = imputeTS::na_ma(flow_na, k = 7, weighting = "exponential")) # mean average over 7 days
-  
-  summary(clean_df)
-  
-  # plot the nas and interpolated versions
-  imputeTS::ggplot_na_distribution(clean_df$flow_na)
-  imputeTS::ggplot_na_distribution(clean_df$flow_kalman)
-  
-  # update plot
-  (g1 <-ggplot() + geom_col(data=clean_df, aes(x=date, y=flow_rev), color=ifelse(clean_df$flow_rev>0, "blue", "red")) +
-      geom_point(data=clean_df, aes(x=date, y=flow_kalman), fill="gray", pch=21) +
-      geom_point(data=clean_df, aes(x=date, y=flow_ma7), fill="cyan4", pch=22))
-  
-
   # write out
-  readr::write_csv(flowdat, glue("data_raw/raw_dwr_fnf_{siteID}.csv"))
-  
-  # print message!
-  print(glue("Data saved here: 'data_raw/raw_dwr_fnf_{siteID}.csv'"))
-  
-  # quick plot
-  p1 <- ggplot(flowdat) +
-    geom_line(aes(x=date, y=flow)) +
-    labs(title=glue("DWR Full Natural Flow for {siteID}: {min(flowdat$water_year)}-{max(flowdat$water_year)}"))
-  
-  return(p1)
-  
+  write_csv(flowdat, file=glue("data_raw/fnf_daily_dwr_{siteID}_downloaded_cdec.csv"))
+
 }
