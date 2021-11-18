c0ffb9b85cfa7f0190abc423076450a886669704
Ryan Peek <rapeek@ucdavis.edu>
2021-11-18 11:08:46 -0800

    finalize load function

--- scripts/functions/f_load_dwr_fnf.R
+++ scripts/functions/f_load_dwr_fnf.R
@@ -10,13 +10,16 @@ f_load_dwr_fnf <- function(site="TLG") {
   # get raw data ID:
   (dwr_fnf_daily <- contentid::store(glue("data_clean/clean_fnf_daily_dwr_{site}_cdec.csv")))
   (dwr_fnf_ann <- contentid::store(glue("data_clean/clean_fnf_ann_stats_{site}_cdec.csv")))
-  dwr_file <- contentid::resolve("hash://sha256/e38981ce3149756bc3498e84b523caa77eab5bc82b81701f128f21f2c70e0745")
+  dwr_daily_file <- contentid::resolve(dwr_fnf_daily)
+  dwr_annual_file <- contentid::resolve(dwr_fnf_ann)
   
-  # read in data
-  dwr_dat <- read_csv(dwr_file)
+  # read in data, use global super assignment <<
+  dwr_daily_df <<- read_csv(dwr_daily_file, show_col_types = FALSE)
+  dwr_ann_df <<- read_csv(dwr_annual_file, show_col_types = FALSE)
   
   print("Data loading complete.")
   
-  return(dwr_dat)
+  # can use a list
+  #return(list(dwr_daily_df, dwr_ann_df))
   
 }
